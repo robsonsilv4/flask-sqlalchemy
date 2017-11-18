c80100435b84f49fbe05cac9eb368c6e0739ab2b
Robson SIlva <robsonsilv410@gmail.com>
2017-11-18 16:25:40 -0300

    Códigos do vídeo sobre one many
    Com as configs do meu banco

---
+++ one_to_many.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config.from_pyfile('config.cfg')
+db = SQLAlchemy(app) 
+
+class Person(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(20))
+	pets = db.relationship('Pet', backref='owner', lazy='dynamic')
+
+class Pet(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(20))
+	owner_id = db.Column(db.Integer, db.ForeignKey('person.id'))
